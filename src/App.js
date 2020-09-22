915dba0c18abc1098f0870a53cb2d09350703c73
S Dagkalakis <stdagalakis@gmail.com>
2020-09-22 14:35:32 +0200

    Remove broken import

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
-
+import img0 from './img/img0.jpg'
+import img2 from './img/img2.jpg'
 function App() {
   return (
     <div className="App">
-      
-      <AliceCarousel autoPlay autoPlayInterval="3000" style={{ height: '500px' }}>
-            <img src="https://rkb86td9.tinifycdn.com/img/img1.jpg" className="slideimg"/>
-            <img src="https://rkb86td9.tinifycdn.com/img/img1.jpg" className="slideimg"/>
-            <img src="https://rkb86td9.tinifycdn.com/img/img2.jpg" className="slideimg"/>
-            <img src= "https://rkb86td9.tinifycdn.com/img/img3.jpg" className="slideimg"/>
-      </AliceCarousel>
+      <img src = {img0} />
     </div>
   );
 }
