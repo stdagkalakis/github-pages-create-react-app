efda02642841b7923f490ff4723d7f5c5eae7896
S Dagkalakis <stdagalakis@gmail.com>
2020-09-22 15:01:17 +0200

    Cdn update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 import img0 from 'https://nlnctck3.tinifycdn.com/img/img0.jpg'
-import img2 from './img/img2.jpg'
+import img2 from '/img/img2.jpg'
 function App() {
   return (
     <div className="App">
-      <img src = {img0} />
+      <img src = "https://nlnctck3.tinifycdn.com/img/img0.jpg" />
+      <img src = "/img/img2.jpg" />
     </div>
   );
 }
