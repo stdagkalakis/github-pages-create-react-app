3189d0d34871a5120f20cff42eadd4358bfb57c2
S Dagkalakis <stdagalakis@gmail.com>
2020-09-22 15:04:00 +0200

    Updates

---
+++ precache-manifest.c237e3c47d5f870ff5b7f5e444c9d1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af92cce759f4561bafa67deb2b750b53",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "ce9c0e9c5e5f712d14c9",
+    "url": "/github-pages-create-react-app/static/css/main.7a4d4a43.chunk.css"
+  },
+  {
+    "revision": "fc74fe5cd0aa68ec93f8",
+    "url": "/github-pages-create-react-app/static/js/2.ae2b7d86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.ae2b7d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9c0e9c5e5f712d14c9",
+    "url": "/github-pages-create-react-app/static/js/main.e41ec90d.chunk.js"
+  },
+  {
+    "revision": "da68a9729d4c495d03af",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.2753c1e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-pages-create-react-app/precache-manifest.1a9be50dab5d5c5927493ddcbfe39e39.js"
+  "/github-pages-create-react-app/precache-manifest.c237e3c47d5f870ff5b7f5e444c9d1d8.js"
 );
 
 self.addEventListener('message', (event) => {
